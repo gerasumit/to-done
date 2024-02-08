7a12ebcc7469e7b572e28aa2718994e839b87a7a
Sumit Gera <sumit.gera@grabtaxi.com>
2024-02-08 22:33:51 +0530

    Initial Commit

---
+++ ToDone/ToDoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoneApp.swift
+//  ToDone
+//
+//  Created by Sumit Gera on 08/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
